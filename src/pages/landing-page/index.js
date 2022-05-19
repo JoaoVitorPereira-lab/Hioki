40d10809ab7bd98d353398110267c6dd1f9dc755
João Vitor <v17112005@gmail.com>
2022-05-19 20:41:18 -0300

    alteracoes na lp

--- src/pages/landing-page/index.js
+++ src/pages/landing-page/index.js
@@ -3,11 +3,16 @@ import './style-320.scss';
 import './style-360.scss';
 import './style-568.scss';
 import './style-768.scss';
+import './style-800.scss';
+import './style-880.scss';
 import './style-960.scss';
 import './style-1080.scss';
 import './style-1140.scss';
 import './style-1280.scss';
 import './style-1366.scss';
+import './style-1440.scss';
+import './style-1680.scss';
+import './style-1780.scss';
 import './style-1920.scss';
 
 export default function LP() {
