ab6e63914b111b3444ba10cb3ee12ccc30cd1d22
Victor Martins Fonseca <victor.fonseca0@gmail.com>
2022-06-19 14:17:11 -0300

    arrumado bugs na api

--- site/src/pages/cadastro/index.js
+++ site/src/pages/cadastro/index.js
@@ -14,7 +14,7 @@ import './style-1440.scss';
 import './style-1680.scss';
 import './style-1780.scss';
 import './style-1920.scss';
-import {cadastrarAgendamento, alterarAgendamento, BuscarPorID, EnviarEmail} from '../../api/agendamentoApi'
+import {cadastrarAgendamento, alterarAgendamento, BuscarPorID} from '../../api/agendamentoApi'
 import { Helmet } from 'react-helmet';
 
 import { ToastContainer, toast } from 'react-toastify';
